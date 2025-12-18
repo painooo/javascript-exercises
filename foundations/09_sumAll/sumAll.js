8d90eca91a866930d7a44b38c5cf69cd55b53193
pyr <jzhangnv@gmail.com>
2025-12-17 21:33:48 -0500

    Finished js foundation exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+// 2, 3, 4
+// 9
+// Find range between first and second number
+// 4, 3, 2, 1 (4, 1)
+// 1, 2, 3, 4 (1, 4)
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (((typeof start) == 'number' && (typeof end) == 'number') && (start > 0 && end > 0) && (!(start % 1) && !(end % 1))) {
+        let range = [start, end].sort((a,b) => a-b);
+        for (range[0]; range[0] <= range[1]; range[0]++){
+            sum+= range[0];
+        }
+    } else {
+        sum='ERROR';
+    }
+    return sum;
 };
 
 // Do not edit below this line
